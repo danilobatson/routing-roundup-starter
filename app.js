48da075ad8b2e13f914022f6f32610e9ddb49dfb
danilobatson <djbatson19@gmail.com>
2021-09-08 12:50:18 -0700

    started pug

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ const express = require('express');
 // Create the Express app.
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.all('*', (req, res) => {
-	res.send('Hello from Express!');
+	// res.send('Hello from Express!');
+  console.log(`Request method: ${req.method}`);
+	console.log(`Request path: ${req.path}`);
+
+
+  const pageData = {
+    title: 'Welcome',
+  heading: 'Home',
+  method: 'GET',
+  path: '/',
+  randomNumber: '9'
+   };
+
+   res.render('layout', pageData);
 });
 
+
+
 const port = 8081
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
