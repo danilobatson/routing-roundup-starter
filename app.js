c203fdf210adc9d99ad19524882da557aadbad67
danilobatson <djbatson19@gmail.com>
2021-09-08 14:36:34 -0700

    passed all test

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
@@ -18,79 +20,54 @@ app.get(/xyz$/, (req, res) => {
   console.log(`Request method: ${req.method}`);
 	console.log(`Request path: ${req.path}`);
 });
-
-app.get(/^\/\w+-*\w*\/\w+/img, (req, res) => {
-  const test = req.path
-  const wordSplit = test.split("/")
-  console.log(wordSplit[2])
-  res.send(wordSplit[2].toUpperCase());
-
+// /^\/\w+-*\w*\/\w+/gim;
+app.get('/capital-letters/:letters', (req, res) => {
+  res.send(req.params.letters.toUpperCase());
   console.log(`Request method: ${req.method}`);
 	console.log(`Request path: ${req.path}`);
 });
 
+app.get('/margot/:letters', (req, res) => {
+  const word = capitalizeFirstLetter(req.params.letters)
+	res.send(word);
+	console.log(`Request method: ${req.method}`);
+	console.log(`Request path: ${req.path}`);
+});
 
+app.get('/margeaux/:letters', (req, res) => {
+	const word = capitalizeFirstLetter(req.params.letters);
+	res.send(word);
+	console.log(`Request method: ${req.method}`);
+	console.log(`Request path: ${req.path}`);
+});
 
-
-
-// /capital-letters/abc
-// 1) should return the plain text response "ABC"
-
-// /capital-letters/asdf
-// 2) should return the plain text response "ASDF"
-
-// /margot/bio
-// 3) should return the plain text response "Bio"
-
-// /margot/contact
-// 4) should return the plain text response "Contact"
-
-// /margeaux/bio
-// 5) should return the plain text response "Bio"
-
-// /margeaux/contact
-// 6) should return the plain text response "Contact"
-
-app.all(/^\/[a-zA-Z-_]+$/ig, (req, res) => {
+app.all(/^\/[a-zA-Z-_]+$/, (req, res) => {
   // res.send('That\'s all I wrote.');
   console.log(`Request method: ${req.method}`);
 	console.log(`Request path: ${req.path}`);
   const pageData = {
     title: 'Welcome',
   heading: 'Home',
-  method: `${req.method}`,
-  path: `${req.path}`,
+  method: req.method,
+  path: req.path,
   randomNumber: 9
    };
   res.render('layout', pageData);
 })
 
-// 1) /capital-letters/abc
-// should return the plain text response "ABC":
-// /capital-letters/asdf
-// /margeaux/bio
-// /margeaux/contact
-
-// app.get('/about_xyz', (req, res) => {
-//   res.send('That\'s all I wrote.');
-//   console.log(`Request method: ${req.method}`);
-// 	console.log(`Request path: ${req.path}`);
-// })
-
-// app.get('/about-xyz', (req, res) => {
-//   res.send('That\'s all I wrote.');
-//   console.log(`Request method: ${req.method}`);
-// 	console.log(`Request path: ${req.path}`);
-// })
-
-
-// app.all('*', (req, res) => {
-// 	res.send('Hello from Express!');
-//   console.log(`Request method: ${req.method}`);
-// 	console.log(`Request path: ${req.path}`);
-// });
-
-
+app.all('/about2', (req, res) => {
+	// res.send('That\'s all I wrote.');
+	console.log(`Request method: ${req.method}`);
+	console.log(`Request path: ${req.path}`);
+	const pageData = {
+		title: 'Welcome',
+		heading: 'Home',
+		method: req.method,
+		path: req.path,
+		randomNumber: 9,
+	};
+	res.render('layout', pageData);
+})
 
 const port = 8081
 
